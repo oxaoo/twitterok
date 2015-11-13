128ac6d94e8492d7d379d00a0d498a797867e865
oxaoo <yol0317@gmail.com>
2015-11-13 23:34:14 +0300

    Implemented receive free port for the deployment of the application; added check the size of the incoming message.

--- src/main/java/CServer.java
+++ src/main/java/CServer.java
@@ -4,111 +4,166 @@ import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.web.handler.sockjs.BridgeEvent;
 import io.vertx.ext.web.handler.sockjs.BridgeOptions;
 import io.vertx.ext.web.handler.sockjs.PermittedOptions;
 import io.vertx.ext.web.handler.sockjs.SockJSHandler;
 import org.json.simple.JSONObject;
 
+import java.io.IOException;
 import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.UnknownHostException;
 import java.text.DateFormat;
 import java.time.Instant;
 import java.util.Date;
 
 import static io.vertx.ext.web.handler.sockjs.BridgeEvent.Type.*;
 
-/**
- * Created by Alexander on 03.11.2015.
- */
 public class CServer extends AbstractVerticle
 {
     private static int m_cntClients = 0;
-    private Logger m_log = LoggerFactory.getLogger(CServer.class);
+    private Logger log = LoggerFactory.getLogger(CServer.class);
+
+    private SockJSHandler mHandler = null;
 
     @Override
     public void start() throws Exception
     {
-        int hostPort = 8080;
-
-        if (Starter.PROCESS_ARGS != null
-                && Starter.PROCESS_ARGS.size() > 0)
+        if (!deploy())
         {
-            try
-            {
-                hostPort = Integer.valueOf(Starter.PROCESS_ARGS.get(0));
-            } catch (NumberFormatException e)
-            {
-                m_log.warn("Invalid port: [" + Starter.PROCESS_ARGS.get(0) + "]");
-            }
+            log.error("Failed to deploy the server.");
+            return;
         }
 
-        m_log.info("Port: " + hostPort);
+        handle();
+    }
+
+    protected boolean deploy()
+    {
+        int hostPort = getFreePort();
+
+        if (hostPort < 0)
+            return false;
 
         Router router = Router.router(vertx);
 
+        //мост шины событий.
+        mHandler = SockJSHandler.create(vertx);
+
+        router.route("/eventbus/*").handler(mHandler);
+        router.route().handler(StaticHandler.create());
+
+        //запуск веб-сервера.
+        vertx.createHttpServer().requestHandler(router::accept).listen(hostPort);
+
+        try
+        {
+            String addr = InetAddress.getLocalHost().getHostAddress();
+            log.info("Access to \"twitterok\" at the following address: \nhttp://" + addr + ":" + hostPort);
+        }
+        catch (UnknownHostException e)
+        {
+            log.error("Failed to get the local address: [" + e.toString() + "]");
+            return false;
+        }
+
+        return true;
+    }
+
+    private int getFreePort()
+    {
+        try
+        {
+            ServerSocket socket = new ServerSocket(0);
+            int port = socket.getLocalPort();
+            socket.close();
+
+            return port;
+        }
+        catch (IOException e)
+        {
+            log.error("Failed to get the free port: [" + e.toString() + "]");
+            return -1;
+        }
+    }
+
+    protected void handle()
+    {
         //назначение адресов для моста шины событий.
         BridgeOptions opts = new BridgeOptions()
                 .addInboundPermitted(new PermittedOptions().setAddress("chat.to.server"))
                 .addOutboundPermitted(new PermittedOptions().setAddress("chat.to.client"));
 
-        //мост шины событий.
-        SockJSHandler ebHandler = SockJSHandler.create(vertx);
-
-
-        ebHandler.bridge(opts, event -> {
-
+        mHandler.bridge(opts, event ->
+        {
             if (event.type() == PUBLISH || event.type() == SEND)
-            {
-                if (event.rawMessage().getString("address").equals("chat.to.server"))
-                {
-                    String message = event.rawMessage().getString("body");
-                    String ip = event.socket().remoteAddress().host();
-                    String time = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.MEDIUM).format(Date.from(Instant.now()));
-
-                    JSONObject jmsg = new JSONObject();
-                    jmsg.put("type", "publish");
-                    jmsg.put("count", m_cntClients);
-                    jmsg.put("time", time);
-                    jmsg.put("addr", ip);
-                    jmsg.put("message", message);
-
-                    String port = String.valueOf(event.socket().remoteAddress().port());
-                    m_log.debug("Publish, ip: " + ip + ", port: " + port);
-
-                    vertx.eventBus().publish("chat.to.client", jmsg.toJSONString());
-                }
-            }
+                publishEvent(event);
 
             //клиент присоединился/покинул чат.
             if (event.type() == SOCKET_CREATED || event.type() == SOCKET_CLOSED)
-            {
-                m_cntClients = (event.type() == SOCKET_CREATED) ? m_cntClients + 1 : m_cntClients - 1;
-
-                String ip = event.socket().remoteAddress().host();
-                String port = String.valueOf(event.socket().remoteAddress().port());
-
-                JSONObject jmsg = new JSONObject();
-                jmsg.put("type", "socket");
-                jmsg.put("count", m_cntClients);
-                jmsg.put("addr", ip);
-                jmsg.put("port", port);
-
-                if (event.type() == SOCKET_CREATED)
-                    m_log.info("Create socket, ip:port [" + ip + ":" + port + "]");
-                else
-                    m_log.info("Closed socket, ip:port [" + ip + ":" + port + "]");
-
-                vertx.eventBus().publish("chat.to.client", jmsg.toJSONString());
-            }
+                socketEvent(event);
 
             event.complete(true);
         });
+    }
 
-        router.route("/eventbus/*").handler(ebHandler);
-        router.route().handler(StaticHandler.create());
+    protected boolean publishEvent(BridgeEvent event)
+    {
+        if (event.rawMessage().getString("address").equals("chat.to.server"))
+        {
+            String message = event.rawMessage().getString("body");
+            if (!verifyMessage(message))
+                return false;
 
-        //запуск вер-сервера.
-        vertx.createHttpServer().requestHandler(router::accept).listen(hostPort);
+            String ip = event.socket().remoteAddress().host();
+            String port = String.valueOf(event.socket().remoteAddress().port());
 
-        m_log.info("Access to \"twitterok\" at the following address: \nhttp://" + InetAddress.getLocalHost().getHostAddress() + ":" + hostPort);
+            // TODO время.
+            String time = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.MEDIUM).format(Date.from(Instant.now()));
+
+            JSONObject jmsg = new JSONObject();
+            jmsg.put("type", "publish");
+            jmsg.put("count", m_cntClients);
+            jmsg.put("time", time);
+            jmsg.put("addr", ip);
+            jmsg.put("message", message);
+
+
+            log.debug("Publish, ip: " + ip + ", port: " + port);
+
+            vertx.eventBus().publish("chat.to.client", jmsg.toJSONString());
+            return true;
+        }
+        else
+            return false;
+    }
+
+    protected boolean socketEvent(BridgeEvent event)
+    {
+        m_cntClients = (event.type() == SOCKET_CREATED) ? m_cntClients + 1 : m_cntClients - 1;
+
+        String ip = event.socket().remoteAddress().host();
+        String port = String.valueOf(event.socket().remoteAddress().port());
+
+        JSONObject jmsg = new JSONObject();
+        jmsg.put("type", "socket");
+        jmsg.put("count", m_cntClients);
+        jmsg.put("addr", ip);
+        jmsg.put("port", port);
+
+        if (event.type() == SOCKET_CREATED) log.info("Create socket, ip:port [" + ip + ":" + port + "]");
+        else log.info("Closed socket, ip:port [" + ip + ":" + port + "]");
+
+        vertx.eventBus().publish("chat.to.client", jmsg.toJSONString());
+        return true;
+    }
+
+    protected boolean verifyMessage(String msg)
+    {
+        if (msg.length() == 0 || msg.length() > 140)
+            return false;
+        else
+            return true;
     }
 }
